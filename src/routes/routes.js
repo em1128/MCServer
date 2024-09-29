14af3a1fd5e1c76bf2149bdddc723e2bf95ac5cf
HomePC <poseidon_x@naver.com>
2024-09-30 01:41:21 +0900

    :lipstick: Design: 서버 상태 정보 및 아이콘 추가

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const mcs = require('node-mcstatus');
 
 const limitMiddle = require('../middlewares/limiterMW');
-const serverService = require('../services/ServerService');
+const serverService = require('../services/serverService');
 const controllers = require('../controllers/controllers');
 
 
@@ -15,16 +15,6 @@ const controllers = require('../controllers/controllers');
 router.get('/run-mcserver', limitMiddle.hardLimiter, serverService.run);
 // .lnk 파일을 실행하는 라우트
 router.get('/backup-world', limitMiddle.softLimiter, serverService.backup);
-
-router.get('/secret-kill', (req, res) => {
-    findAndKillProcessWindows('java.exe');
-    res.send('kill server');
-});
-
-router.get('/secret-off', (req, res) => {
-    shutdownComputer();
-    res.send('PC off');
-});
 router.get('/check-server', limitMiddle.softLimiter, async (req, res) => {
 
   // 서버 상태 확인
@@ -44,6 +34,40 @@ router.get('/check-server', limitMiddle.softLimiter, async (req, res) => {
       console.error('서비스 오류:', error);
     });
 
+});
+router.get('/check-server2', limitMiddle.softLimiter, async (req, res) => {
+
+    // 서버 상태 확인
+    mcs.statusJava(process.env.HOST_IP, process.env.HOST_PORT)
+      .then((result) => {
+        if (result.online) {
+          const msg = `서버가 온라인 상태입니다! 플레이어 수: ${result.players.online}/${result.players.max}`;
+          console.log(msg);
+          res.json({
+              online: result.online,
+              players: result.players.online
+          });
+        } else {
+          const msg = '서버가 오프라인 상태입니다.'
+          console.log(msg);
+          res.json({
+              online: result.online
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('서비스 오류:', error);
+      });
+  
+  });
+router.get('/secret-kill', (req, res) => {
+    findAndKillProcessWindows('java.exe');
+    res.send('kill server');
+});
+
+router.get('/secret-off', (req, res) => {
+    shutdownComputer();
+    res.send('PC off');
 });
 
 // Windows에서 프로세스 목록을 가져오고 PID를 파싱하여 종료하기
