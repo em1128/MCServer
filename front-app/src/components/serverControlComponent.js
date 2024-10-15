15e630cfb65f2ba1a4d929cf3a164f699bbe2381
em1128 <75158913+em1128@users.noreply.github.com>
2024-10-15 18:36:27 +0900

    Update serverControlComponent.js
    PC방에서 임시로

--- front-app/src/components/serverControlComponent.js
+++ front-app/src/components/serverControlComponent.js
@@ -25,7 +25,7 @@ const ServerControlComponent = () => {
         };
     }, []);
 
-    // 버튼 클릭 시 호출되는 함수
+    // 접속 확인 클릭 시 호출되는 함수
     const handleFetchStatus = async () => {
         try {
             const data = await fetchGameStatus();
@@ -38,13 +38,31 @@ const ServerControlComponent = () => {
         }
     };
     
+    // 서버 열기 클릭 시 호출되는 함수
+    const handleRunServer = async () => {
+        try {
+            const data = await runServer();
+        } catch (error) {
+            setErrorMessage('데이터를 가져오는 중 오류가 발생했습니다.');
+        }
+    };
+
+    // 월드 백업 클릭 시 호출되는 함수
+    const handleBackupWorld = async () => {
+        try {
+            const data = await backupWorld();
+        } catch (error) {
+            setErrorMessage('데이터를 가져오는 중 오류가 발생했습니다.');
+        }
+    };
+    
     return (
         <div class="server-control">
             <h2 class="title"><img id="star" src="1.png" alt=""/> 마크 서버 관리기</h2>
             
             <div class="button-group">
-                <button class="server-button" id="run-mcserver">서버 열기</button>
-                <button class="server-button" id="backup-world">월드 백업</button>
+                <button class="server-button" id="run-mcserver" onClick={handleRunServer}>서버 열기</button>
+                <button class="server-button" id="backup-world" onClick={handleBackupWorld}>월드 백업</button>
                 <button class="server-button" id="check-server" onClick={handleFetchStatus}>접속 확인</button>
             </div>
             {/* 자식 컴포넌트로 상태 전달 */}
